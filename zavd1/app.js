f71ff33bd929910037d64433d5fd51aeeb22970d
Yura <55669434+yurakorabel@users.noreply.github.com>
2021-09-28 00:04:44 +0300

    Add files via upload

---
+++ zavd1/app.js
@@ -0,0 +1,3 @@
+for(let i = 1; i > 0; i*i) {
+  console.log(i*i);
+}
---
+++ zavd2/app.js
@@ -0,0 +1,7 @@
+let text = 'yura';
+let uppercase = text.toUpperCase();
+let first_letter = uppercase[0].toLowerCase();
+let rest_of_text = first_letter + uppercase.slice(1);
+
+console.log(rest_of_text);
+
